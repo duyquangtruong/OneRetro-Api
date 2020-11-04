1a2d9a084880c9c4e926239b8fe02e241ad208b9
Trương Quang Duy <truongquangduy343@gmail.com>
2020-11-04 16:56:45 +0700

    done

--- one-retro-api/app.js
+++ one-retro-api/app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const mongoose = require("mongoose");
 const Uri =
@@ -14,15 +16,16 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/login", require("./routes/login/index"));
 app.use("/boards", require("./routes/boards/index"));
-app.use(require("cors"));
 
 //Create connection to database
 const connectDatabase = () => {
--- one-retro-api/routes/login/index.js
+++ one-retro-api/routes/login/index.js
@@ -4,15 +4,35 @@ const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const User = require("../../models/users.model");
 const dotenv = require("dotenv");
+const { request } = require("express");
 dotenv.config();
 const SecretKey = process.env.SECRET_KEY;
 
-/* GET home page. */
-router.get("/", async function (req, res, next) {
-  let token = jwt.sign(JSON.stringify({ name: "duyquangtruong" }), SecretKey);
-  let result = jwt.verify(token, SecretKey);
-  console.log(token);
-  res.send(result);
+router.post("/", async function (req, res, next) {
+  let token = jwt.sign(
+    JSON.stringify({
+      username: req.body.username,
+      password: req.body.password,
+    }),
+    SecretKey
+  );
+
+  console.log(req.body);
+  const user = await User.findOne({ username: req.body.username });
+
+  if (user == null) {
+    res.json({ error: 400 });
+    return;
+  }
+
+  if (user.password !== token) {
+    res.json({ error: 401 });
+    return;
+  }
+
+  let isRemember = req.body.isRemember;
+
+  res.json(user);
 });
 
 module.exports = router;
